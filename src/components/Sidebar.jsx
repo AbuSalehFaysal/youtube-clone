79182a4998f89d8c986f1378b8b81bb814747b27
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2022-09-24 13:14:37 +0600

    feed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants";
+import { Category } from "@mui/icons-material";
 
 const Sidebar = () => {
+  const selectedCategory = "New";
   return (
-    <div>Sidebar</div>
-  )
-}
+    <Stack
+      direction="row"
+      sx={{
+        overflow: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button className="category-btn" style={{background: category.name === selectedCategory && "#FC1503", color: "#fff"}} key={category.name}>
+          <span style={{color: category.name === selectedCategory ? "white" : "red", marginRight: "15px"}}>{category.icon}</span>
+          <span style={{opacity: category.name === selectedCategory ? "1" : "0.8"}}>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
